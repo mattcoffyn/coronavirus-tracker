d74cdd344dbbe45105b5985b692950c2b76e744a
Matt Coffyn <mattcoffyn@matts-mbp.home>
2021-03-06 16:42:49 +0000

    try again?

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `//url.to/covid19-graphql.herokuapp.com`;
-export const prodEndpoint = `//url.to/covid19-graphql.herokuapp.com`;
+export const endpoint = `https://covid19-graphql.herokuapp.com`;
+export const prodEndpoint = `https://covid19-graphql.herokuapp.com`;
 export const perPage = 4;
